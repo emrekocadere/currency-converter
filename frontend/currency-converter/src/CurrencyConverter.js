8da2686581d7e6734dde55a050cc9dd9f3f0df43
emrekocadere <emrekocadere34@gmail.com>
2024-11-12 20:20:08 +0300

    Feat: enhance `CurrencyConverter` layout with additional styling and components

--- frontend/currency-converter/src/CurrencyConverter.js
+++ frontend/currency-converter/src/CurrencyConverter.js
@@ -1,55 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
-import { InputNumber,Select,DatePicker } from 'antd';
+import { InputNumber, Select, DatePicker, Button } from 'antd';
 
 function CurrencyConverter() {
   return (
-    <div className="App" style={{background:"rgb(246, 246, 246)",width:"60vw"}}>
-    <DatePicker style={{ width: '200px', height: '40px' }} />
-    <InputNumber min={0} defaultValue={3} />
-    <Select
-    showSearch
-    placeholder="Select a person"
-    filterOption={(input, option) =>
-      (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
-    }
-    options={[
-      {
-        value: '1',
-        label: 'Jack',
-      },
-      {
-        value: '2',
-        label: 'Lucy',
-      },
-      {
-        value: '3',
-        label: 'Tom',
-      },
-    ]}
-  />
-    <InputNumber min={0} defaultValue={3} />
-    <Select
-    showSearch
-    placeholder="Select a person"
-    filterOption={(input, option) =>
-      (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
-    }
-    options={[
-      {
-        value: '1',
-        label: 'Jack',
-      },
-      {
-        value: '2',
-        label: 'Lucy',
-      },
-      {
-        value: '3',
-        label: 'Tom',
-      },
-    ]}
-  />
+    <div className="App" style={{ background: "rgb(246, 246, 246)", width: "40vw", borderRadius: "25px", boxShadow: "5px 5px 5px 5px rgb(222, 222, 222)", display: "flex", flexDirection:"row", justifyContent: "space-between", padding:"3vh 3vw"}}>
+
+      {/* <DatePicker style={{ width: '200px', height: '40px' }} /> */}
+
+
+      <div style={{ display: "flex",flexDirection:"column"  }}>
+        <InputNumber min={0} defaultValue={3} size={'large'} style={{ marginBottom: "20px", width: "15vw",height:"5vh" }} />
+        <Select
+          showSearch
+          style={{  width: "15vw",height:"5vh" }}
+          placeholder="Select a person"
+          filterOption={(input, option) =>
+            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+          }
+          options={[
+            {
+              value: '1',
+              label: 'Jack',
+            },
+            {
+              value: '2',
+              label: 'Lucy',
+            },
+            {
+              value: '3',
+              label: 'Tom',
+            },
+          ]}
+        />
+      </div>
+      <div style={{ padding: "3vw" }}>
+        <Button >emre</Button>
+      </div>
+
+
+      <div style={{ display: "flex",flexDirection:"column" }}>
+        <InputNumber min={0} defaultValue={3} size={'large'} style={{ marginBottom: "20px", width: "15vw",height:"5vh" }} />
+        <Select
+          showSearch
+          style={{  width: "15vw",height:"5vh" }}
+          placeholder="Select a person"
+          filterOption={(input, option) =>
+            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+          }
+          options={[
+            {
+              value: '1',
+              label: 'Jack',
+            },
+            {
+              value: '2',
+              label: 'Lucy',
+            },
+            {
+              value: '3',
+              label: 'Tom',
+            },
+          ]}
+        />
+      </div>
+
 
     </div>
   );
