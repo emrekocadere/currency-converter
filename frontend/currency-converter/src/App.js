05e724c1fe40ef62f4377c33b5725bbc0bd60a3c
emrekocadere <emrekocadere34@gmail.com>
2024-11-13 15:54:44 +0300

    Feat: add `CustomFooter` component with social media buttons and developer credit

--- frontend/currency-converter/src/App.js
+++ frontend/currency-converter/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import CurrencyConverter from './CurrencyConverter';
 import { Flex, Layout } from 'antd';
+import NewsCard from './NewsCard';
+import CustomFooter from './CustomFooter';
 const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-<CurrencyConverter></CurrencyConverter>
+<CustomFooter></CustomFooter>
   );
 }
 
