ab69c12aaf7b3a07f68c66289ec5d23267d80324
emrekocadere <emrekocadere34@gmail.com>
2024-11-15 16:55:14 +0300

    Fix: correct GetNewsFromDb function and refactor to async/await

--- frontend/currency-converter/src/apiService.js
+++ frontend/currency-converter/src/apiService.js
@@ -3,31 +3,36 @@ import axios from 'axios';
 
 
 export function ConvertCurrency(requestbody) {
-
+console.log("sdfsd")
     axios.post('http://localhost:5203/api/CurrencyConverter/ConvertCurrency',
         requestbody)
         .then(function (response) {
             console.log(response);
+            
         })
         .catch(function (error) {
             console.log(error);
         });
 }
 
-export function GetNewsFromDb() {
+export async function GetNewsFromDb() {
 
-    axios.get('http://localhost:5203/api/CurrencyConverter/GetNewsFromDb')
-    .then(function (response) {
-      // handle success
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
-    });
+  // await axios.get('http://localhost:5203/api/CurrencyConverter/GetNewsFromDb')
+  //   .then(function (response) {
+  //     // handle success
+  //     console.log(response);
+  //     return "ne haber";
+  //   })
+  //   .catch(function (error) {
+  //     // handle error
+  //     console.log(error);
+  //   });
+
+
+
+  const response = await axios.get('http://localhost:5203/api/CurrencyConverter/GetNewsFromDb');
+        console.log(response);
+        return response.data; 
 }
 
 
