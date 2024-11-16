2fb93bd6ab3c6c48aa203cee3a197d2379664cfe
emrekocadere <emrekocadere34@gmail.com>
2024-11-17 02:17:05 +0300

    Style: adjust alignment of footer buttons to start

--- frontend/currency-converter/src/CustomFooter.js
+++ frontend/currency-converter/src/CustomFooter.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { Button,Divider } from 'antd';
+import { Button, Divider } from 'antd';
 import { GithubOutlined, LinkedinOutlined, MailOutlined } from '@ant-design/icons';
 
 function CustomFooter() {
     return (
-        <div style={{ background: "rgb(255, 128, 0)",display:"flex",flexDirection:"row", justifyContent: "center", alignItems: "center" }}>
+        <div style={{ background: "rgb(255, 128, 0)", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
 
 
-            <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ display: "flex", flexDirection: "column",alignItems:"flex-start" }}>
                 <Button color="default" variant="link"><LinkedinOutlined />LinkedIn</Button>
                 <Button color="default" variant="link"><GithubOutlined />Github</Button>
                 <Button color="default" variant="link"><MailOutlined />Email</Button>
-               
+
             </div>
-            <Divider type="vertical" style={{height:"10vh"}} />
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <p style={{ color: "white" }}>Developed By Salih Emre Kocadere</p>
+
+            <Divider type="vertical" style={{ height: "10vh" }} />
+
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <p style={{ color: "white" }}>Developed By Salih Emre Kocadere</p>
             </div>
-           
+
 
         </div>
     );
