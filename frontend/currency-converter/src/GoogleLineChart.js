0a86da4c0f5326a1b75e87a93d2b806195a21436
emrekocadere <emrekocadere34@gmail.com>
2024-11-20 02:13:45 +0300

    Feat: enhance GoogleLineChart to dynamically render data from currency rates prop

--- frontend/currency-converter/src/GoogleLineChart.js
+++ frontend/currency-converter/src/GoogleLineChart.js
@@ -1,32 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Chart } from "react-google-charts";
 
-const data = [
-  ["x", "dogs"],
-  [0, 0],
-  [1, 10],
-  [2, 23],
-  [3, 17],
-  [4, 18],
-  [5, 9],
-  [6, 11],
-  [7, 27],
-  [8, 33],
-  [9, 40],
-  [10, 32],
-  [11, 35],
-];
+// const data = [
+//   ["x", "dogs"],
+//   [0, 0],
+//   [1, 10],
+//   [2, 23],
+//   [3, 17],
+//   [4, 18],
+//   [5, 9],
+//   [6, 11],
+//   [7, 27],
+//   [8, 33],
+//   [9, 40],
+//   [10, 32],
+//   [11, 35],
+// ];
+
+
 
 const options = {
-  title: "Line Chart Example",
   hAxis: { title: "Time" },
   vAxis: { title: "Popularity" },
   legend: "none",
 };
 
-function GoogleLineChart() {
+function GoogleLineChart(props) {
+
+  
+  const [data, setData] = useState(["x", "EUR"]);
+
+  function convertRatesToData() {
+   
+    const dates = Object.keys(props.currencyRates);
+   let currency = Object.keys(props.currencyRates[dates[0]])[0]; 
+    const newData = [["x", currency]]; 
+   
+
+    dates.forEach((date, index) => {
+     // const currency=Object.keys(props.currencyRates[date])[0];
+      const value = props.currencyRates[date][currency];
+      newData.push([date, value]);
+    });
+
+    console.log(newData);  // Log the new data
+    setData(newData);  // Update the state with the new data
+  }
+
+useEffect(()=>{console.log(props.currencyRates); convertRatesToData();},[props.currencyRates])
+
   return (
-    <div style={{ }}>
+    <div style={{}}>
       <Chart
         chartType="LineChart"
         width="100%"
