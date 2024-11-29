68d8df7cd44067c44b13a9ed5c102e0f90cf5253
emrekocadere <emrekocadere34@gmail.com>
2024-11-29 16:20:21 +0300

    Feat: integrate dynamic currency rate data into Google Line Chart

--- frontend/currency-converter/src/GoogleLineChart.js
+++ frontend/currency-converter/src/GoogleLineChart.js
@@ -1,34 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Chart } from "react-google-charts";
 
-// const data = [
-//   ["x", "dogs"],
-//   [0, 0],
-//   [1, 10],
-//   [2, 23],
-//   [3, 17],
-//   [4, 18],
-//   [5, 9],
-//   [6, 11],
-//   [7, 27],
-//   [8, 33],
-//   [9, 40],
-//   [10, 32],
-//   [11, 35],
-// ];
+const datas = [
+  ["x", "dogs"],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+];
 
 
 
 const options = {
-  hAxis: { title: "Time" },
-  vAxis: { title: "Popularity" },
-  legend: "none",
+  hAxis: { title: "Date" },
+  vAxis: { title: "Worth" },
+  legend:"none",
+  colors: ["rgb(239,135,51)"] 
 };
 
 function GoogleLineChart(props) {
 
   
-  const [data, setData] = useState(["x", "EUR"]);
+  const [data, setData] = useState(datas);
 
   function convertRatesToData() {
    
@@ -47,13 +41,15 @@ function GoogleLineChart(props) {
     setData(newData);  // Update the state with the new data
   }
 
-useEffect(()=>{console.log(props.currencyRates); convertRatesToData();},[props.currencyRates])
+
+
+useEffect(()=>{convertRatesToData() },[props.currencyRates])
 
   return (
     <div style={{}}>
       <Chart
         chartType="LineChart"
-        width="100%"
+        width="30vw"
         height="40vh"
         data={data}
         options={options}
