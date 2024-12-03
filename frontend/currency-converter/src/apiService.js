369503d0d8a31b256b3b2bbd028f1491dbc8660e
emrekocadere <emrekocadere34@gmail.com>
2024-12-03 13:48:11 +0300

    Feat: add API methods for currency rates

--- frontend/currency-converter/src/apiService.js
+++ frontend/currency-converter/src/apiService.js
@@ -2,41 +2,78 @@ import axios from 'axios';
 
 
 
-export function ConvertCurrency(requestbody) {
-console.log("sdfsd")
-    axios.post('http://localhost:5203/api/CurrencyConverter/ConvertCurrency',
-        requestbody)
-        .then(function (response) {
-            console.log(response);
-            
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+// export function ConvertCurrency(requestbody) {
+// console.log("sdfsd")
+//     axios.post('http://localhost:5203/api/CurrencyConverter/ConvertCurrency',
+//         requestbody)
+//         .then(function (response) {
+//             console.log(response);
+
+
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// }
+
+
+
+export async function ConvertCurrencyAsync(requestbody) {
+
+    const response = await axios.post('http://localhost:5203/api/CurrencyConverter/ConvertCurrency', requestbody)
+    return response
 }
+
 
 export async function GetNewsFromDb() {
 
-  // await axios.get('http://localhost:5203/api/CurrencyConverter/GetNewsFromDb')
-  //   .then(function (response) {
-  //     // handle success
-  //     console.log(response);
-  //     return "ne haber";
-  //   })
-  //   .catch(function (error) {
-  //     // handle error
-  //     console.log(error);
-  //   });
+    // await axios.get('http://localhost:5203/api/CurrencyConverter/GetNewsFromDb')
+    //   .then(function (response) {
+    //     // handle success
+    //     console.log(response);
+    //     return "ne haber";
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    //   });
 
 
 
-  const response = await axios.get('http://localhost:5203/api/CurrencyConverter/GetNewsFromDb');
-        console.log(response);
-        return response.data; 
+    const response = await axios.get('http://localhost:5203/api/CurrencyConverter/GetNewsFromDb');
+    console.log(response);
+    return response.data;
 }
 
 
 
+export async function GetCurrenciesAsync() {
+
+    const response =await axios.get('http://localhost:5203/api/CurrencyConverter/GetCurrencies')
+    console.log(response)
+    return response.data
+}
+
+// export  function GetCurrencies() {
+
+//     const response = axios.get('http://localhost:5203/api/CurrencyConverter/GetCurrencies')
+//     console.log(response)
+//     return response
+// }//Promise {<pending>}
 
 
 
+
+export async function GetConvertCurrencyRatesAsync(requestbody) {
+
+    const response =await axios.post('http://localhost:5203/api/CurrencyConverter/GetConvertCurrencyRates',requestbody)
+    console.log(response)
+    return response.data
+}
+
+export async function GetCommonCurrenciesRatesAsync() {
+
+    const response =await axios.get('http://localhost:5203/api/CurrencyConverter/GetCommonCurrencies')
+    console.log(response)
+    return response.data
+}
