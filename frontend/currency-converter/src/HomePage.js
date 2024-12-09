1809da9a8e9f593ab94e60ebf22e2d6194eb0d04
emrekocadere <emrekocadere34@gmail.com>
2024-12-09 20:02:46 +0300

    Feat: integrate responsive layout using react-responsive for improved UX

--- frontend/currency-converter/src/HomePage.js
+++ frontend/currency-converter/src/HomePage.js
@@ -1,35 +1,67 @@
-import './App.css';
-import React from 'react';
-import { Typography } from 'antd';
+import './index.css';
+import React, { useState, useEffect } from 'react';
+import { Typography, Col, Row } from 'antd';
 import CurrencyConverter from './CurrencyConverter';
 import GoogleChart from './GoogleLineChart';
+import NewsPage from './Pages/NewsPage';
+import CommonCurrenciesRates from './CommonCurrenciesRates';
+import { useMediaQuery } from 'react-responsive'
 
 export default function HomePage() {
+
+
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-width: 900px)'
+    })
+    const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
+    const [currencyGraph, setCurrencyGraph] = useState("HomePage");
+
+    function fetchCurrencyData(currencyGraph) {
+        setCurrencyGraph(
+            currencyGraph
+
+        );
+        console.log(currencyGraph)
+    }
     return (
-        <div style={{ paddingInline: "8vw", paddingTop: "2vh" }}>
+        <div className='homePageDiv'>
+            <Row>
+                <Typography.Title level={2} style={{ paddingBottom: "1vh" }}>
+                    Converter
+                </Typography.Title>
+            </Row>
 
-            <div style={{display:"flex",flexDirection:"row"}}>
-                <div >
-                    <Typography.Title level={2} style={{ paddingBottom: "1vh" }}>
-                        Converter
-                    </Typography.Title>
+            {/* Eğer isDesktopOrLaptop ise düzenleme yapılır */}
+            <Row gutter={[16, 16]} style={{ flexDirection: isDesktopOrLaptop ? 'row' : 'column' }}>
+                <Col span={isDesktopOrLaptop ? 12 : 24}>
+                    <CurrencyConverter sendCurrencyData={fetchCurrencyData} />
+                </Col>
+                <Col
+                    span={isDesktopOrLaptop ? 12 : 24}
+                    style={{ display: "flex", justifyContent: "center", marginTop: isDesktopOrLaptop ? "-7vh" : "0" }}
+                >
+                    <GoogleChart currencyRates={currencyGraph}></GoogleChart>
+                </Col>
+            </Row>
 
-                    <CurrencyConverter />
-
-                </div>
-                <div style={{paddingLeft:"100px",width:"100vw"}}>
-                <GoogleChart></GoogleChart>
-                </div>
-               
-            </div>
-
-            <div>
+            <Row>
                 <Typography.Title level={2} style={{ paddingBottom: "1vh" }}>
                     News
                 </Typography.Title>
-            </div>
+            </Row>
 
-
+            <Row gutter={[16, 16]} style={{ flexDirection: isDesktopOrLaptop ? 'row' : 'column' }}>
+                <Col span={isDesktopOrLaptop ? 12 : 24}>
+                    <NewsPage />
+                </Col>
+                <Col span={isDesktopOrLaptop ? 12 : 24}>
+                    <CommonCurrenciesRates />
+                </Col>
+            </Row>
         </div>
     );
 }
