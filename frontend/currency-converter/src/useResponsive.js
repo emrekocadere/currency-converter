3e2a308c01e51fb806385a4b64ec65dbd201d0d5
emrekocadere <emrekocadere34@gmail.com>
2024-12-13 17:06:13 +0300

    Refactor: enhance useResponsive hook for device-specific queries

--- frontend/currency-converter/src/useResponsive.js
+++ frontend/currency-converter/src/useResponsive.js
@@ -1,16 +1,23 @@
 import { useMediaQuery } from 'react-responsive';
 
 export default function useResponsive() {
-    const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 900px)' });
+    const isMobile = useMediaQuery({ query: '(max-width: 480px)' });
+
+    const isTablet = useMediaQuery({ query: '(min-width: 481px) and (max-width: 1024px)' });
+
+    const isDesktop = useMediaQuery({ query: '(min-width: 1025px)' });
+
     const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' });
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+
     const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+
     const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
 
     return {
-        isDesktopOrLaptop,
+        isMobile,
+        isTablet,
+        isDesktop,
         isBigScreen,
-        isTabletOrMobile,
         isPortrait,
         isRetina,
     };
