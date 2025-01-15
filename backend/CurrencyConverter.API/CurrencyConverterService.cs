7ca7afc408f1cb02bed020e6634be7dd43801e9c
emrekocadere <emrekocadere34@gmail.com>
2025-01-15 15:40:02 +0300

    Feat: enhance currency ratio handling

--- backend/CurrencyConverter.API/CurrencyConverterService.cs
+++ backend/CurrencyConverter.API/CurrencyConverterService.cs
@@ -10,19 +10,25 @@ public class CurrencyConverterService
     readonly private HttpClient _httpClient;
     readonly private IConfiguration _configuration;
     readonly private CurrencyConverterDbContext _dbContext;
-    private List<CurrencyRatio>currencyRates;
+    private List<CurrencyRatio> currencyRates;
     public CurrencyConverterService(HttpClient httpClient, IConfiguration configuration, CurrencyConverterDbContext dbContext)
     {
         _httpClient = httpClient;
         _configuration = configuration;
         _dbContext = dbContext;
+        currencyRates = _dbContext.CurrencyRatios.ToList();
     }
 
     public decimal ConvertCurrency(ConvertCurrencyReqDTO dto)
     {
-        var currencyRate=currencyRates.Where(x=>x.Currencies==dto.Currencies).FirstOrDefault();
-        decimal result=dto.Amount*currencyRate.Rate;
+        var currencyRate = currencyRates.Where(x => x.Currencies == dto.Currencies).FirstOrDefault();
+        decimal result = dto.Amount * currencyRate.Rate;
         return result;
+    }
+    public List<CurrencyRatio> GetCurrencyRates()
+    {
+
+        return currencyRates.Where(x=>x.Currencies.StartsWith("USD")).ToList();
     }
 
     public List<Currency> GetCurrencies()
@@ -42,26 +48,40 @@ public class CurrencyConverterService
     }
     public void GetCurrencyRatesFromDb()
     {
-        currencyRates=_dbContext.CurrencyRatios.ToList();
+        currencyRates = _dbContext.CurrencyRatios.ToList();
     }
-    
+
     public void SaveTheCurencyRatesToDb(List<CurrencyRatio> currencyRatioList)
     {
-            _dbContext.CurrencyRatios.AddRange(currencyRatioList);
-            _dbContext.SaveChanges();
+        foreach(var currencyRate in currencyRatioList)
+        {
+                 if(_dbContext.CurrencyRatios.Any(cr => cr.Currencies == currencyRate.Currencies))
+                 {
+                   var currency =  _dbContext.CurrencyRatios.Where(x=>x.Currencies==currencyRate.Currencies).FirstOrDefault();
+                   currency.Rate = currencyRate.Rate;
+
+                 }
+                 else{
+
+                    _dbContext.CurrencyRatios.Add(currencyRate);
+                 }
+        }
+       
+        
+        _dbContext.SaveChanges();
     }
 
-    public async Task<string> GetCurrencyRates(GetCurrencyRatesDTO dto)
-    {
-        var todayDate = DateTime.Today.ToString("yyyy-MM-dd");
-        var pastDate = DateTime.Today.AddMonths(-2).ToString("yyyy-MM-dd");
+    // public async Task<string> GetCurrencyRates(GetCurrencyRatesDTO dto)
+    // {
+    //     var todayDate = DateTime.Today.ToString("yyyy-MM-dd");
+    //     var pastDate = DateTime.Today.AddMonths(-2).ToString("yyyy-MM-dd");
 
-        _httpClient.DefaultRequestHeaders.Add("apikey", _configuration["ApiKey"]);
-        using HttpResponseMessage response = await _httpClient.GetAsync($"https://api.apilayer.com/exchangerates_data/timeseries?start_date={pastDate}&end_date={todayDate}&base={dto.FromCurrency}&symbols={dto.ToCurrency}");
-        var jsonResponse = await response.Content.ReadAsStringAsync();// bak buraya
+    //     _httpClient.DefaultRequestHeaders.Add("apikey", _configuration["ApiKey"]);
+    //     using HttpResponseMessage response = await _httpClient.GetAsync($"https://api.apilayer.com/exchangerates_data/timeseries?start_date={pastDate}&end_date={todayDate}&base={dto.FromCurrency}&symbols={dto.ToCurrency}");
+    //     var jsonResponse = await response.Content.ReadAsStringAsync();// bak buraya
 
-        return jsonResponse;
-    }
+    //     return jsonResponse;
+    // }
 
 
 
