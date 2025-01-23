db60aa937aba53dc81daf691f7395b9c3eb528f1
emrekocadere <emrekocadere34@gmail.com>
2025-01-23 17:57:35 +0300

    Feat: add migration to update CurrencyRatesTimestamps schema and adjust data types

--- backend/CurrencyConverter.API/Migrations/20250122201923_UpdateCurrenyRatesTimestamps.Designer.cs
+++ backend/CurrencyConverter.API/Migrations/20250122201923_UpdateCurrenyRatesTimestamps.Designer.cs
@@ -4,6 +4,7 @@ using CurrencyConverter.API;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CurrencyConverter.API.Migrations
 {
     [DbContext(typeof(CurrencyConverterDbContext))]
-    partial class CurrencyConverterDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250122201923_UpdateCurrenyRatesTimestamps")]
+    partial class UpdateCurrenyRatesTimestamps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/CurrencyConverter.API/Migrations/20250122201923_UpdateCurrenyRatesTimestamps.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyConverter.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCurrenyRatesTimestamps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Timestamp",
+                table: "CurrencyRatesTimestamps",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rate",
+                table: "CurrencyRatesTimestamps",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currencies",
+                table: "CurrencyRatesTimestamps",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Timestamp",
+                table: "CurrencyRatesTimestamps",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Rate",
+                table: "CurrencyRatesTimestamps",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currencies",
+                table: "CurrencyRatesTimestamps",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
