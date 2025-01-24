a6f376c20372d84eaadcb7deb41c95b1713f37dc
emrekocadere <emrekocadere34@gmail.com>
2025-01-25 00:31:01 +0300

    Refactor: remove async modifier from action methods in CurrencyConverterController

--- backend/CurrencyConverter.API/Controllers/CurrencyConverterController.cs
+++ backend/CurrencyConverter.API/Controllers/CurrencyConverterController.cs
@@ -15,13 +15,13 @@ namespace CurrencyConverter.API.Controllers
             this.service = service;
         }
         [HttpPost("ConvertCurrency")]
-        public async Task<ActionResult> ConvertCurrency(ConvertCurrencyReqDTO dto)
+        public  ActionResult ConvertCurrency(ConvertCurrencyReqDTO dto)
         {
             return Ok(service.ConvertCurrency(dto));
         }
 
         [HttpGet("GetCurrencies")]
-        public async Task<ActionResult> GetCurrencies()
+        public ActionResult GetCurrencies()
         {
             return Ok(service.GetCurrencies());
         }
@@ -33,21 +33,21 @@ namespace CurrencyConverter.API.Controllers
         }
 
         [HttpGet("GetCurrencyRates")]
-        public async Task<ActionResult> GetCurrencyRates(string currentCurrency)
+        public  ActionResult GetCurrencyRates(string currentCurrency)
         {
-            var response = service.GetCurrencyRates(currentCurrency);
+            var response =  service.GetCurrencyRates(currentCurrency);
             return Ok(response);
         }
 
         [HttpGet("GetCurrencyRatesForThreeMonths")]
-        public async Task<ActionResult> GetCurrencyRatesForThreeMonths(string currencies)
+        public  ActionResult GetCurrencyRatesForThreeMonths(string currencies)
         {
             var response = service.GetCurrencyRatesForThreeMonths("currencies");
             return Ok(response);
         }
 
         [HttpPost("ConvertCurrencyForSpecificDate")]
-        public async Task<ActionResult> ConvertCurrencyForSpecificDate(ConvertCurrencyForSpecificDateDTO dto)
+        public ActionResult ConvertCurrencyForSpecificDate(ConvertCurrencyForSpecificDateDTO dto)
         {
             var response = service.ConvertCurrencyForSpecificDate(dto);
             return Ok(response);
