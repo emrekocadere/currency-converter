d0b24cd31a436481297500eb32468feb3f5553a8
emrekocadere <emrekocadere34@gmail.com>
2025-02-04 17:54:40 +0300

    Feat: add DbSet for VisitingUser and ErrorLog entities in CurrencyConverterDbContext

--- backend/CurrencyConverter.API/CurrencyConverterDbContext.cs
+++ backend/CurrencyConverter.API/CurrencyConverterDbContext.cs
@@ -9,7 +9,10 @@ public class CurrencyConverterDbContext : DbContext
     public CurrencyConverterDbContext(DbContextOptions<CurrencyConverterDbContext> options) : base(options) { }
     public DbSet<Currency> Currencies { get; set; }
     public DbSet<News> News { get; set; }
-    public DbSet<CurrencyRatio>CurrencyRatios { get; set; }
+    public DbSet<CurrencyRatio> CurrencyRatios { get; set; }
     public DbSet<CurrencyRatesTimestamp> CurrencyRatesTimestamps { get; set; }
+    public DbSet<VisitingUser> VisitingUserss { get; set; }
+    public DbSet<ErrorLog> ErrorLogs { get; set; }
+
 
 }
