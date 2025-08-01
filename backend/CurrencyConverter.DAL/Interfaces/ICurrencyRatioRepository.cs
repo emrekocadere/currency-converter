11d6af5b3dc34361f46fe554c8b3b875ba112211
emrekocadere <emrekocadere34@gmail.com>
2025-08-01 12:39:37 +0300

    feat: add CurrencyRatioRepository and ICurrencyRatioRepository

---
+++ backend/CurrencyConverter.DAL/Interfaces/ICurrencyRatioRepository.cs
@@ -0,0 +1,9 @@
+using System;
+using CurrencyConverter.API.Entities;
+
+namespace CurrencyConverter.DAL.Interfaces;
+
+public interface ICurrencyRatioRepository:IRepository<CurrencyRatio>
+{
+    CurrencyRatio FindByName(string name);
+}
---
+++ backend/CurrencyConverter.DAL/Repositories/CurrencyRatioRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using CurrencyConverter.API.Entities;
+using CurrencyConverter.DAL.Interfaces;
+using Microsoft.VisualBasic;
+
+namespace CurrencyConverter.DAL.Repositories;
+
+public class CurrencyRatioRepository : Repository<CurrencyRatio>, ICurrencyRatioRepository
+{
+    
+    public CurrencyRatioRepository(CurrencyConverterDbContext dbContext) : base(dbContext)
+    {
+    }
+    public CurrencyRatio FindByName(string name)
+    {
+        var currencyRatio = _context.CurrencyRatios.FirstOrDefault(x => x.Currencies == name);
+        if(currencyRatio == null)
+        {
+            throw new Exception($"Currency ratio with name {name} not found."); 
+        }
+        return currencyRatio;
+    }
+}
