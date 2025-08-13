a57e27385b5619b827065f9db21b0806ab7b1473
emrekocadere <emrekocadere34@gmail.com>
2025-08-13 15:35:54 +0300

    refactor: export currencyImages and improve CommonCurrenciesRates rendering

--- frontend/currency-converter/src/CommonCurrenciesRates.js
+++ frontend/currency-converter/src/CommonCurrenciesRates.js
@@ -12,7 +12,7 @@ import Chf from "./Images/ch.svg"
 
 const { Title, Paragraph, Text, Link } = Typography;
 
-const currencyImages = {
+export const currencyImages = {
   USD: Usd,
   EUR: Eur,
   CAD: Cad,
@@ -20,24 +20,26 @@ const currencyImages = {
   CHF: Chf,
   JPY: Jpy,
   GBP: Gbp
-};
 
+};
+let int = -1;
 function CommonCurrenciesRates(props) {
+
   const [currencyGraph, setCurrencyGraph] = useState("HomePage");
   const [currenyRates, setCurrenyRates] = useState([]);
 
 
 
   async function GetCommonCurrenciesRates() {
-    let response = await getRatesForCurrency(props.currentBaseCurrency);
+    let response = await getRatesForCurrency(props.currentBaseCurrency)
     //  console.log(response.data)
-    setCurrenyRates(response.data);
+    setCurrenyRates(response.data)
   }
 
 
   useEffect(() => {
-    GetCommonCurrenciesRates();
-  }, [props.currentBaseCurrency]);
+    GetCommonCurrenciesRates()
+  }, [props.currentBaseCurrency])
 
 
   return (
@@ -45,39 +47,41 @@ function CommonCurrenciesRates(props) {
 
       <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", backgroundColor: "rgba(239, 135, 51,1)" , borderRadius: "15px", paddingInline: "10px" }}>
 
-        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-          <img className='country-flag' src={currencyImages[props.currentBaseCurrency]} alt={props.currentBaseCurrency} />
-          <p style={{ marginInline: ".5vw", fontSize: "1.2em" }}>{props.currentBaseCurrency}</p>
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }} >
+
+          <img className='country-flag' src={currencyImages[props.currentBaseCurrency]}/>
+          
+          <p style={{ marginInline: ".5vw", fontSize: "1.2em" }} >{props.currentBaseCurrency} </p>
+
         </div>
 
-        <p style={{ marginInline: ".5vw", fontSize: "1.2em" }}>1</p>
+          <p style={{marginInline:".5vw" , fontSize:"1.2em"}} > 1</p>
 
       </div>
-      {currenyRates.map((item, index) => {
-        let targetCurrency = item.currencies.slice(3, 6);
-        return (
-          <div 
-            style={{ 
-              display: "flex", 
-              flexDirection: "row", 
-              justifyContent: "space-between", 
-              backgroundColor: index % 2 === 0 ? "rgba(255, 255, 255, 0.5)" : "rgba(239, 135, 51,.8)", 
-              borderRadius: "15px", 
-              paddingInline: "10px" 
-            }} 
-            key={index}
-          >
-            <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-              <img className='country-flag' src={currencyImages[targetCurrency]} alt={targetCurrency} />
-              <p style={{ marginInline: ".5vw", fontSize: "1.2em", fontWeight: "300" }}>{targetCurrency}</p>
+      {
+        currenyRates.map((item, index) => {
+          int++
+          let targetCurrency = item.currencies.slice(3, 6);
+          return (
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", backgroundColor: int % 2 === 0 ?"rgba(255, 255, 255, 0.5)"  : "rgba(239, 135, 51,.8)", borderRadius: "15px", paddingInline: "10px" }} key={index}>
+
+              <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }} >
+
+                <img className='country-flag' src={currencyImages[targetCurrency]} />
+                <p style={{ marginInline: ".5vw", fontSize: "1.2em",fontWeight:"300" }} > {targetCurrency} </p>
+        
+
+              </div>
+
+              <div>
+                <p style={{ fontSize: "1.2em",fontWeight:"500" }}>{item.rate}</p>
+              </div>
+
             </div>
 
-            <div>
-              <p style={{ fontSize: "1.2em", fontWeight: "500" }}>{item.rate}</p>
-            </div>
-          </div>
-        );
-      })}
+          )
+        })
+      }
     </div>
 
   );
