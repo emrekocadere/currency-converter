5e20270d0c532a308304b7d7d6390f0f874f0996
emrekocadere <emrekocadere34@gmail.com>
2025-10-16 20:39:02 +0300

    refactor: refactor header layout and update action buttons

--- frontend/currency-converter/src/CustomHeader.js
+++ frontend/currency-converter/src/CustomHeader.js
@@ -1,28 +1,54 @@
-import logo from './logo.svg';
 import './index.css';
 import React from 'react';
-import { Button, ConfigProvider, Divider } from 'antd';
-import { GithubOutlined, LinkedinOutlined, MailOutlined } from '@ant-design/icons';
+import { Button, ConfigProvider } from 'antd';
+import { GithubOutlined, LinkedinOutlined, CoffeeOutlined } from '@ant-design/icons';
 import imagee from "./Images/Logo.png"
+
 function CustomHeader() {
     return (
         <ConfigProvider
             theme={{
-                components: {
-                    Divider: {
-            
-                    }
+                token: {
+                    colorPrimary: '#ff8000',
                 }
             }}
         >
-            <div className='custom-header' >
-                <div>
-                    <img className="header-img" src={imagee} ></img>
-
+            <header className='custom-header'>
+                <div className='header-inner'>
+                    <div className='brand'>
+                        <img className="header-img" src={imagee} alt="Currency Converter" />
+                    </div>
+                    <div className='header-actions'>
+                        <Button 
+                            type="text"
+                            icon={<GithubOutlined />}
+                            href="https://github.com/emrekocadere/currency-converter"
+                            target="_blank"
+                            className="custom-header-github-button"
+                        >
+                            GitHub
+                        </Button>
+                        <Button 
+                            type="primary"
+                            icon={<LinkedinOutlined />}
+                            href="https://www.linkedin.com/in/salih-emre-kocadere-7a61b0203/"
+                            target="_blank"
+                            className="custom-header-linkedin-button"
+                        >
+                            LinkedIn
+                        </Button>
+                        <Button 
+                            type="default"
+                            icon={<CoffeeOutlined />}
+                            href="https://www.buymeacoffee.com/emrekocadere"
+                            target="_blank"
+                            className="custom-header-coffee-button"
+                        >
+                            Buy me a coffee
+                        </Button>
+                    </div>
                 </div>
-
-                <Divider className='divider' />
-            </div>
+            </header>
         </ConfigProvider>
     );
 }
