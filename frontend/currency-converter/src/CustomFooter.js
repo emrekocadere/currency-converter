13a25f0a1dd3381cecd6b1a4bae2863ef1387a83
emrekocadere <emrekocadere34@gmail.com>
2025-10-16 20:38:27 +0300

    refactor: refactor and enhance CustomFooter component

--- frontend/currency-converter/src/CustomFooter.js
+++ frontend/currency-converter/src/CustomFooter.js
@@ -1,36 +1,53 @@
-import logo from './logo.svg';
 import './index.css';
 import React from 'react';
 import { Button, Divider } from 'antd';
-import { GithubOutlined, LinkedinOutlined, MailOutlined } from '@ant-design/icons';
+import { GithubOutlined, LinkedinOutlined, MailOutlined, HeartFilled } from '@ant-design/icons';
 import useResponsive from './useResponsive';
 
 function CustomFooter() {
     const { isMobile } = useResponsive();
     return (
-        <div className='custom-footer-div' >
+        <footer className='custom-footer-div'>
+            <div className={`custom-footer-content ${isMobile ? 'custom-footer-content-mobile' : 'custom-footer-content-desktop'}`}>
+                <div className='custom-footer-links-div'>
+                    <Button 
+                        type="link" 
+                        className='cutom-footer-links' 
+                        href='https://www.linkedin.com/in/salih-emre-kocadere-7a61b0203/'
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        icon={<LinkedinOutlined />}
+                    >
+                        LinkedIn
+                    </Button>
+                    <Button 
+                        type="link" 
+                        className='cutom-footer-links' 
+                        href='https://github.com/emrekocadere'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        icon={<GithubOutlined />}
+                    >
+                        GitHub
+                    </Button>
+                    <Button 
+                        type="link" 
+                        className='cutom-footer-links'
+                        icon={<MailOutlined />}
+                    >
+                        Email
+                    </Button>
+                </div>
 
+                {!isMobile && <Divider type="vertical" className="custom-footer-divider" />}
 
-            <div className='custom-footer-links-div'  >
-                <Button color="default" variant="link" className='cutom-footer-links' href='https://www.linkedin.com/in/salih-emre-kocadere-7a61b0203/'         target="_blank" 
-        rel="noopener noreferrer">
-                    <LinkedinOutlined />LinkedIn
-                </Button>
-                <Button color="default" variant="link" className='cutom-footer-links' href='https://github.com/emrekocadere'>
-                    <GithubOutlined />Github
-                </Button>
-                <Button color="default" variant="link" className='cutom-footer-links'><MailOutlined />Email</Button>
-     
+                <div className="custom-footer-credits">
+                    <span>Made with</span>
+                    <HeartFilled className="custom-footer-heart" />
+                    <span>by Salih Emre Kocadere</span>
+                </div>
             </div>
-
-            < Divider className="custom-footer-divider" type={isMobile ? "horizontal" : "vertical"} />
-
-            <div style={{ display: "flex", alignItems: "center", color: "white", fontSize: "80%" }}>
-                <p >Developed By Salih Emre Kocadere</p>
-            </div>
-
-
-        </div>
+        </footer>
     );
 }
 
