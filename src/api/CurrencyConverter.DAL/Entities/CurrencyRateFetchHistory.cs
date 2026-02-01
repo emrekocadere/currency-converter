793553d0fa792d188031d39617d380c9b56fe9a9
emrekocadere <emrekocadere34@gmail.com>
2026-02-01 16:32:01 +0300

    feat: add Currencies column to fetch history

--- src/api/CurrencyConverter.DAL/Entities/CurrencyRateFetchHistory.cs
+++ src/api/CurrencyConverter.DAL/Entities/CurrencyRateFetchHistory.cs
@@ -5,6 +5,7 @@ namespace CurrencyConverter.DAL.Entities;
 public class CurrencyRateFetchHistory
 {
     public int Id { get; set; }
+    public required string Currencies { get; set; }
     public DateOnly date { get; set; }
 
 }
---
+++ src/api/CurrencyConverter.DAL/Persistence/Migrations/20260201105746_AddCurrenciesColumnToRateFetchHistoryTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyConverter.DAL.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrenciesColumnToRateFetchHistoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Currencies",
+                table: "CurrencyRateFetchHistories",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currencies",
+                table: "CurrencyRateFetchHistories");
+        }
+    }
+}
