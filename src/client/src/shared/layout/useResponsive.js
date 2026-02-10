b3e4b0d65376de5a2c71ee6e34526843d65d4b5f
emrekocadere <emrekocadere34@gmail.com>
2026-02-10 19:25:14 +0300

    chore: brand app and update public assets

--- src/client/src/shared/layout/useResponsive.js
+++ src/client/src/shared/layout/useResponsive.js
@@ -1,7 +1,7 @@
 import { useMediaQuery } from 'react-responsive';
 
 export function useResponsive() {
-    const isMobile = useMediaQuery({ query: '(max-width: 480px)' });
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
     const isTablet = useMediaQuery({ query: '(min-width: 769px) and (max-width: 1500px)' });
     const isDesktop = useMediaQuery({ query: '(min-width: 1280px) and (max-width: 1920px) and (min-height: 720px) and (max-height: 1080px)' });
     const isBigScreen = useMediaQuery({ query: '(min-width: 1921px)' });
